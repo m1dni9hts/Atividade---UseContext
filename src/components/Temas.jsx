72985274b9af52786f5d7b87155f4c79ef21af6a
Laura <pizzuttilaura@gmail.com>
2024-07-14 16:26:02 -0300

    commit

--- src/components/Temas.jsx
+++ src/components/Temas.jsx
@@ -3,14 +3,14 @@ import React, { createContext, useState } from 'react';
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light');
+  const [tema, setTema] = useState('light');
 
   const toggleTheme = () => {
-    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+    setTheme((prevTema) => (prevTema === 'light' ? 'dark' : 'light'));
   };
 
   return (
-    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+    <ThemeContext.Provider value={{ tema, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
