ad25598f381cc30b3ebcbc99c2285bcd337e03b5
Laura <pizzuttilaura@gmail.com>
2024-07-14 16:23:11 -0300

    commit

--- src/ThemedApp.js
+++ src/ThemedApp.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { ThemeContext } from './ThemeContext';
+import { ThemeContext } from '../src/components/Temas';
 import './App.css'; // para estilos
 
 const ThemedApp = () => {
