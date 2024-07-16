b07e8c5a83b6cd9b7d66b572879cf78d4fc72fd9
Laura <pizzuttilaura@gmail.com>
2024-07-16 11:06:28 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-      </header>
+      <h1>HELLO WORLD</h1>
     </div>
   );
 }
